d15c5a610b90a97a3d667246bcf19fedd0079982
tineojf <92743762+tineodev@users.noreply.github.com>
2024-03-10 15:45:12 -0500

    Add Tools class

---
+++ src/entregable/logic/Tools.java
@@ -0,0 +1,10 @@
+package entregable.logic;
+
+public class Tools {
+
+    public static void activarModal(javax.swing.JLabel _label, String _text) {
+        _label.setText(_text);
+        _label.setVisible(true);
+        _label.setEnabled(true);
+    }
+}
