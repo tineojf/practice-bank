461a1d1aa43eb93e9c0797596b4d471f7b2560a6
tineojf <92743762+tineodev@users.noreply.github.com>
2024-03-14 09:38:40 -0500

    Fix typo

--- src/entregable/gui/Display.java
+++ src/entregable/gui/Display.java
@@ -193,7 +193,7 @@ public class Display extends javax.swing.JFrame {
     private void mostrarOperaciones() {
         boolean salir = false;
         do {
-            String opcion = JOptionPane.showInputDialog(null, "1. Consulta\n2. Retiro\n3.Deposito\n4. Salir");
+            String opcion = JOptionPane.showInputDialog(null, "1. Consulta\n2. Retiro\n3. Deposito\n4. Salir");
 
             switch (opcion) {
                 case "1":
