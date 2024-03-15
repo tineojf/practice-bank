9f488049fa57ef35a30b957fa4bedf1bd98afdcf
tineojf <92743762+tineodev@users.noreply.github.com>
2024-03-14 21:30:19 -0500

    inicializate db in main class

--- src/entregable/Entregable.java
+++ src/entregable/Entregable.java
@@ -1,10 +1,13 @@
 package entregable;
 
 import entregable.gui.Login;
+import entregable.persistence.DB;
 
 public class Entregable {
 
     public static void main(String[] args) {
+        DB baseDatos = DB.getInstance();
+
         Login inicio = new Login();
         inicio.setVisible(true);
         inicio.setLocationRelativeTo(null);
